3216ceba01c4f0ef7768c41ce826466d273daed8
Leo <leo07carrasco@gmail.com>
2022-09-29 16:14:31 -0300

    new proyect

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,36 @@
+
+
+const dice = document.querySelector('.dice-icon'); 
+const id_advice = document.querySelector('#id-advice');
+const advice = document.querySelector('#advice');
+
+console.log(id_advice)
+
+// fetch('https://api.adviceslip.com/advice')
+// .then((response) => {
+//     return response.json()
+// } )
+// .then((data) => {
+//     let obj = data;
+//     console.log(obj.slip.id)
+
+//     id_advice.innerHTML = '#'+obj.slip.id+'';
+
+// })
+
+
+dice.addEventListener('click', ()=>{
+    
+    fetch('https://api.adviceslip.com/advice')
+    .then((response) => {
+        return response.json()
+    } )
+    .then((data) => {
+        let obj = data;
+        console.log(obj.slip.id)
+    
+        id_advice.innerHTML = '#'+obj.slip.id+'';
+        advice.innerHTML = '"'+obj.slip.advice+'"';
+    
+    })    
+})
