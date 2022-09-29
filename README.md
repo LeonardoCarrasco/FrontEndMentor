25c770901fb785274a97d6c1c2e99f00ecc31074
Leo <leo07carrasco@gmail.com>
2022-09-29 16:17:37 -0300

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## [intereactive-card-detail-form](https://leonardocarrasco.github.io/FrontEndMentor/Intereactive-card-detail-form/index.html)
 
 ## [intro-section-with-dropdown-navigation](intro-section-with-dropdown-navigation-main\index.html)
+
+## [advice-generator-app-main](advice-generator-app-main\index.html)
--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,22 +1,8 @@
 
-
 const dice = document.querySelector('.dice-icon'); 
 const id_advice = document.querySelector('#id-advice');
 const advice = document.querySelector('#advice');
 
-console.log(id_advice)
-
-// fetch('https://api.adviceslip.com/advice')
-// .then((response) => {
-//     return response.json()
-// } )
-// .then((data) => {
-//     let obj = data;
-//     console.log(obj.slip.id)
-
-//     id_advice.innerHTML = '#'+obj.slip.id+'';
-
-// })
 
 
 dice.addEventListener('click', ()=>{
